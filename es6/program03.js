01d98bd682c9bb7cb5ddc07ae1c4a4114fbec64d
s_fujita <chaashuuu@gmail.com>
2015-09-15 18:55:28 +0900

    Add program03 and writing

---
+++ es6/program03.js
@@ -0,0 +1,56 @@
+// ES6
+
+var util = require('util');
+class Character {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.health_ = 100;
+	}
+
+
+	damage() {
+		this.health_ - 10;
+	}
+
+	getHealth() {
+		return this.health_;
+	}
+
+	toString() {
+		return "x: " + this.x + " y: " + this.y + " health: " + this.getHealth();
+	}
+
+};
+
+class Player extends Character {
+	constructor(x, y, name) {
+		super.call(this, x, y);
+		this.name = name;
+	}
+
+	util.inherits(Player, Character);
+
+	move(dx, dy) {
+		this.x += dx;
+		this.y += dy;
+	}
+
+	toString() {
+		return "name: " + this.name + " " + Player.toString.call(this);
+	}
+};
+
+// あかんわからん。
+// superとutilのとこがダメ。
+
+var x = process.argv[2];
+var y = process.argv[3];
+var name = process.argv[4];
+var character = new Character(+x, +y);
+character.damage();
+console.log(character.toString());
+var player = new Player(+x, +y, name);
+player.damage();
+player.move(7, 8);
+console.log(player.toString());
